3dd75faf0b6594bfdbf3a2426bf6e2a45d35225e
MartinoSanchez <121335052+MartinoSanchez@users.noreply.github.com>
2022-12-27 17:19:20 +0100

    Add button to current rider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,4 +22,8 @@ export class AppComponent {
     "Karl",
     "Louis"
   ]
+
+  addRider() {
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RiderComponent } from './rider/rider.component';
-import { TableComponent } from './table/table.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {RiderComponent} from './rider/rider.component';
+import {TableComponent} from './table/table.component';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RiderComponent,
-    TableComponent
+    TableComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
